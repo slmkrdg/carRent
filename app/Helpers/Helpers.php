cf1877220dcdcbc58b1e7696be43e9a7cc5ba646
Selim Karadağ <selimkaradag93@gmail.com>
2024-12-10 08:37:30 +0300

    Update Helpers.php

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,17 +1,6 @@
 <?php
 
-/**
- * Pattern for the response to be given in API requests
- *
- * @param int $resultType [0 = Success, 1 = Error, 2 = Logout, 3 = Update, 4 = Refresh]
- * @param int $errorType [0 = Success, 1 = Error, 2 = Warning, 3 = Info]
- * @param int $responseCode
- * @param string $message
- * @param array $data [Object]
- *
- * @return mixed
- */
-function generateApiMessage(int $resultType, int $errorType, int $responseCode ,string $errorMessage = "", $data = null)
+function generateResponse(int $resultType, int $errorType, int $responseCode ,string $errorMessage = "", $data = null)
 {
     return response()->json([
         'resultType'    => $resultType,
